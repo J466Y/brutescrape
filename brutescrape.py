48554b7f0e37023736af65d8929224fd46bc3e8b
Jaggy <61787998+J466Y@users.noreply.github.com>
2023-09-28 17:04:01 +0200

    Updated for Python3 & QoL improvements

--- brutescrape.py
+++ brutescrape.py
@@ -2,19 +2,27 @@
 
 #Secure Planet LLC
 
-import urllib2
+import urllib.request as request
 import re
 import re
 import os, sys
 
 from collections import OrderedDict
 
+
+if len(sys.argv) > 3 or len(sys.argv) < 2:
+    print ("""[!]Error: Invalid syntax:
+    [*]Ussage:  brutescrape.py [site_list.scrape] 
+                brutescrape.py [site_list.scrape] <OPTIONAL> [OutputPassFile.txt] """)
+    exit(1)
+
+
 banner_file = "brutescrape.banner"
 def banner():
     global banner_file
     open_banner = open(banner_file, "r")
     for line in open_banner:
-        print line.rstrip()
+        print (line.rstrip())
     open_banner.close()
 
 def stripHTMLTags (html):
@@ -58,8 +66,8 @@ banner()
 y_arr = []
 
 try:
-    file_list = open('sites.scrape','r')
-    sites = file_list.read().split(',')
+    file_list = open(sys.argv[1],'r')
+    sites = file_list.read().split('\n')
 
 except:
     banner()
@@ -68,11 +76,11 @@ except:
 for site in sites:
     try:
         site = site.strip()
-        print "[*] Downloading Content For : " + site
+        print ("[*] Downloading Content For : " + site)
         x_arr = []
-        response = urllib2.urlopen(site)
+        response = request.urlopen(site)
         response.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0')]
-        x = stripHTMLTags(response.read())
+        x = stripHTMLTags(response.read().decode('utf-8'))
 	#Replace junk found in our response
         x = x.replace('\n',' ')
         x = x.replace(',',' ')
@@ -87,12 +95,19 @@ for site in sites:
                 y = y[2:]
               y_arr.append(y)
     except Exception as e:
-        print "[*] Error: " + str(e)
+        print ("[*] Error: " + str(e))
         pass
 
 y_arr_unique = OrderedDict.fromkeys(y_arr).keys()
-print "[*] Processing List"
-f_write = open("passwordList.txt","w")
+print ("[*] Processing List")
+if len(sys.argv) > 2:
+ 
+    f_write = open(sys.argv[2],"w")
+else:
+
+    f_write = open("passwordList.txt","w")
+
+    
 for yy in y_arr_unique:
     if yy.strip().isdigit():
       pass
@@ -100,6 +115,6 @@ for yy in y_arr_unique:
       #print yy.strip()
       f_write.write(yy.strip() + "\n")
 f_write.close()
-print "[*] Wordlist Generation Complete."
-print "[*] Output Located: passwordList.txt"
-print "[*] Total Count of Passwords >> " + str(len(y_arr_unique))
+print ("[*] Wordlist Generation Complete.")
+print ("[*] Output Located: " + f_write.name)
+print ("[*] Total Count of Passwords >> " + str(len(y_arr_unique)))
